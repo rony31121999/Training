e7937ff7564d3369468f5d6444cedd6d774687dd
rony31121999 <112923035+rony31121999@users.noreply.github.com>
2022-09-23 16:42:59 +0530

    Add files via upload

---
+++ stringr.java
@@ -0,0 +1,10 @@
+public class stringr {
+    public static void main(String[] args) {
+        String len="Ronak";
+        int i=0;
+        for(i=len.length()-1;i>=0;i--){
+            System.out.print(len.charAt(i));
+        }
+    }
+}
+
