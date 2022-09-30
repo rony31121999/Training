140e8a083bf6a1a78d2403e3f8a23c49fcc0c63e
rony31121999 <112923035+rony31121999@users.noreply.github.com>
2022-09-30 11:44:39 +0530

    Add files via upload

---
+++ set1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class set1{
+    public static void main(String[] args) {
+    SortedSet<String>node2=new TreeSet<>();
+        node2.add("fd");
+        node2.add("qaw");
+        node2.add("asee");
+        node2.add("qwe");
+        System.out.println(node2);
+        }
+    }
