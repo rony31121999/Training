6874c229a75595776f5f176b0564bc1b1a2398f0
rony31121999 <112923035+rony31121999@users.noreply.github.com>
2022-09-30 11:33:47 +0530

    Add files via upload

---
+++ set.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+public class set {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> node=new LinkedHashSet<>();
+    HashSet<Integer>node1=new HashSet<>();
+        node.add(1);
+        node.add(3);
+        node.add(null);
+        node.add(65);
+        node.add(null);
+        node1.add(1);
+        node1.add(3);
+        node1.add(null);
+        node1.add(65);
+        node1.add(null);
+        System.out.println(node);
+        System.out.println(node1);
+        }
+    }
