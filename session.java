0e81fdc8444783c05ef05753b4a6fa79db5d5ec3
rony31121999 <112923035+rony31121999@users.noreply.github.com>
2022-10-04 16:16:12 +0530

    Add files via upload

---
+++ session.java
@@ -0,0 +1,20 @@
+class session extends Thread {
+    public void run() {
+        for(int i = 0; i<10; i++){
+            System.out.println(i +Thread.currentThread().getName());
+        }
+        System.out.println("Iam custom thread:" +Thread.currentThread().getName());
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        session t1 = new session();
+        System.out.println("I am main thread:"+Thread.currentThread().getName());
+        t1.setName("sum");
+        t1.start();
+       t1.join();
+        for(int i = 0; i<10; i++){
+            System.out.println(i +Thread.currentThread().getName());
+        }
+    }
+}
+
