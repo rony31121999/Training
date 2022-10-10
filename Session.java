aec6a001a7b3ceb09785b50aaa80a7daf5f188c8
rony31121999 <112923035+rony31121999@users.noreply.github.com>
2022-10-10 10:44:54 +0530

    Update Session.java

--- Session.java
+++ Session.java
@@ -1,4 +1,4 @@
-class session extends Thread {
+class Session extends Thread {
     public void run() {
         for(int i = 0; i<10; i++){
             System.out.println(i +Thread.currentThread().getName());
