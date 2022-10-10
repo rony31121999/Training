244309b2ef2aa1782ca6d0e646283193eaeaa325
rony31121999 <112923035+rony31121999@users.noreply.github.com>
2022-10-10 10:41:38 +0530

    Update Employeehashmap.java

--- Employeehashmap.java
+++ Employeehashmap.java
@@ -1,33 +1,33 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class employee {
+class Employeehashmap {
     int id;
     String name;
 
 
-    public employee(int i, String n) {
+    public Employeehashmap(int i, String n) {
         this.id = i;
         this.name = n;
 
     }
 
 }
-        class entry1 {
-            public static void main(String args[]) {
-                employee q = new employee(1, "dhiru");
-                employee w = new employee(23, "roank");
-                employee e = new employee(112, "praj");
-                employee r = new employee(938, "div");
-                employee t = new employee(12, "red");
-                HashMap<Integer, employee> empmap = new HashMap();
-                empmap.put(1, q);
-                empmap.put(23, w);
-                empmap.put(112, e);
-                empmap.put(938, r);
-                empmap.put(12, t);
-                System.out.println(empmap);
-            }
-        }
+class Employeehashmap1 {
+    public static void main(String args[]) {
+        Employeehashmap q = new Employeehashmap(1, "dhiru");
+        Employeehashmap w = new Employeehashmap(23, "roank");
+        Employeehashmap e = new Employeehashmap(112, "praj");
+        Employeehashmap r = new Employeehashmap(938, "div");
+        Employeehashmap t = new Employeehashmap(12, "red");
+        HashMap<Integer, Employeehashmap> empmap = new HashMap();
+        empmap.put(1, q);
+        empmap.put(23, w);
+        empmap.put(112, e);
+        empmap.put(938, r);
+        empmap.put(12, t);
+        System.out.println(empmap);
+    }
+}
 
 
